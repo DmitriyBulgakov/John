474c3446db4588d3f2fc0de70fe12b6d8738a15b
Bulgakov Dmitriy <dmitriybulgakov@gmail.com>
2022-09-18 12:11:16 +0200

    and of the project

--- js/script.js
+++ js/script.js
@@ -1,1 +1,8 @@
 "use strict";
+const menu = document.querySelector('.menu__body');
+const iconBotton = document.querySelector('.icon-menu');
+
+iconBotton.addEventListener('click', () => {
+iconBotton.classList.toggle('_active');
+menu.classList.toggle('_active');
+})
